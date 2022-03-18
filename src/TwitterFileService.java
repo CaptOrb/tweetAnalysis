9d6a9f4742a4ed326f6d1c36a3f849c9039b449c
jobrien <jane.obrien@ucdconnect.ie>
2022-03-18 23:14:14 +0000

    tidied up some code, added Main class

--- src/TwitterFileService.java
+++ src/TwitterFileService.java
@@ -4,7 +4,6 @@ import twitter4j.User;
 import java.io.*;
 
 public class TwitterFileService {
-
     public void writeTweet(Status tweet, boolean retweet) throws IOException {
         File file = new File("s.txt");  // this is a file handle, s.txt may or may not exist
         boolean foundInFile = false;
@@ -25,28 +24,47 @@ public class TwitterFileService {
             try (PrintWriter pw = new PrintWriter(new FileWriter(file, true))) {
                 String tweetText;
                 if(retweet)
-                     tweetText = tweet.getRetweetedStatus().getText();
+                    tweetText = tweet.getRetweetedStatus().getText();
                 else
                     tweetText = tweet.getText();
 
                 pw.println(tweet.getId() + "\t"
-                            + "@" + tweet.getUser().getScreenName() + "\t"
-                            + tweetText.replaceAll("\n", " ") + "\t"
-                            + tweet.getRetweetCount() + "\t"
-                            + tweet.getCreatedAt());
+                        + "@" + tweet.getUser().getScreenName() + "\t"
+                        + tweetText.replaceAll("\n", " ") + "\t"
+                        + tweet.getRetweetCount() + "\t"
+                        + tweet.getCreatedAt());
             }
         }
     }
 
     public void writeUser(User user) throws IOException {
         File file = new File("users.txt");
+        boolean found = false;
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null)  // classic way of reading a file line-by-line
+                if (line.equals("@" + user.getScreenName() + "\t"
+                        + user.getLocation() + "\t"
+                        + user.getDescription().replaceAll("\n", " ") + "\t"
+                        + user.getFollowersCount())){
 
-        try (PrintWriter pw = new PrintWriter(new FileWriter(file, true))) {
+                    found = true;
+
+                    break;  // if the text is present, we do not have to read the rest after all
+                }
+        } catch (FileNotFoundException fnfe) {
+            fnfe.printStackTrace();
+        }
+
+
+        if(!found) {
+            try (PrintWriter pw = new PrintWriter(new FileWriter(file, true))) {
                 pw.println("@" + user.getScreenName() + "\t"
                         + user.getLocation() + "\t"
                         + user.getDescription().replaceAll("\n", " ") + "\t"
                         + user.getFollowersCount());
             }
-
         }
+
     }
+}
