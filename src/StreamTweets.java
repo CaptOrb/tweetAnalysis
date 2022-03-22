6c76842bd5651d8b2219a6c20c9d201a8f71d1f1
tom <tom.higgins@ucdconnect.ie>
2022-03-22 12:46:25 +0000

    setup of StreamTweets.java

--- src/StreamTweets.java
+++ src/StreamTweets.java
@@ -1,2 +1,35 @@
+import twitter4j.*;
+
 public class StreamTweets {
+    StatusListener listener = new StatusListener() {
+        @Override
+        public void onException(Exception e) {
+
+        }
+
+        @Override
+        public void onStatus(Status status) {
+
+        }
+
+        @Override
+        public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+        }
+
+        @Override
+        public void onTrackLimitationNotice(int i) {
+
+        }
+
+        @Override
+        public void onScrubGeo(long l, long l1) {
+
+        }
+
+        @Override
+        public void onStallWarning(StallWarning stallWarning) {
+
+        }
+    };
+
 }
