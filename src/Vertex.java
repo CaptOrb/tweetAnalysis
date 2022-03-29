9031b570f0ba463fcf879b82edc250cfbeccb908
conor.barry <conor.barry@ucdconnect.ie>
2022-03-29 12:15:58 +0100

    start graph

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,7 +1,28 @@
-public class Vertex<T> {
+public class Vertex<T extends Comparable<T>> implements Graph<T> {
     String label;
     Vertex(String label) {
         this.label = label;
     }
+
+    @Override
+    public void addVertex(Vertex vertex) {
+
+    }
+
+    @Override
+    public void removeVertex(Vertex vertex) {
+
+    }
+
+    @Override
+    public void addEdge(Vertex vertex1, Vertex vertex2) {
+
+    }
+
+    @Override
+    public void removeEdge(Vertex vertex1, Vertex vertex2) {
+
+    }
 }
+
 
