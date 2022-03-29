fb1d54ad423972b40f1b91d282f66501d36f3e9d
conor.barry <conor.barry@ucdconnect.ie>
2022-03-29 12:16:48 +0100

    start graph

---
+++ src/RetweetGraph.java
@@ -0,0 +1,22 @@
+public class RetweetGraph implements Graph {
+
+    @Override
+    public void addVertex(Vertex vertex) {
+
+    }
+
+    @Override
+    public void removeVertex(Vertex vertex) {
+
+    }
+
+    @Override
+    public void addEdge(Vertex vertex1, Vertex vertex2) {
+
+    }
+
+    @Override
+    public void removeEdge(Vertex vertex1, Vertex vertex2) {
+
+    }
+}
