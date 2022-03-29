126907c86907df6e5052b4156fe1ca11f4cf57db
conor.barry <conor.barry@ucdconnect.ie>
2022-03-29 12:20:07 +0100

    remove redundant code

--- src/RetweetGraph.java
+++ src/RetweetGraph.java
@@ -1,4 +1,17 @@
+import java.util.List;
+import java.util.Map;
+
 public class RetweetGraph implements Graph {
+
+    private Map<Vertex, List<Vertex>> adjVertices;
+
+    public Map<Vertex, List<Vertex>> getAdjVertices() {
+        return adjVertices;
+    }
+
+    public void setAdjVertices(Map<Vertex, List<Vertex>> adjVertices) {
+        this.adjVertices = adjVertices;
+    }
 
     @Override
     public void addVertex(Vertex vertex) {
