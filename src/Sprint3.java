5c59cbea1eb3ee7b4732205e9c7da0fcc42bf29f
jobrien <jane.obrien@ucdconnect.ie>
2022-03-29 17:32:25 +0100

    adds username and the user they retweeted to an arraylist, this must be converted to an adjaceny list and an int of how many times the user retweeted that user

---
+++ src/Sprint3.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+
+public class Sprint3 {
+
+    public static void main(String[] args) {
+
+        Configuration configuration = new Configuration();
+        try {
+            // user chooses to provide a config file as a command arg
+            if (args.length == 1) {
+                configuration.getSettingsFromFile(configuration, args[0], 1);
+            } else {
+                // config file is on class path
+                configuration.getSettingsFromFile(configuration, "config_file", 0);
+            }
+
+            File dataFile = new File(configuration.getDataDirectory(), configuration.getDataFile());
+
+            GraphFormation graphFormation = new GraphFormation();
+
+            if (dataFile.exists()) {
+                graphFormation.readRetweetsIntoSet(dataFile);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
