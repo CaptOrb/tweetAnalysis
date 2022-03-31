38a756d01664163a94a1e285203189443c06d438
conor.barry <conor.barry@ucdconnect.ie>
2022-03-31 17:41:42 +0100

    write line by line to graph output file

--- src/RetweetFileService.java
+++ src/RetweetFileService.java
@@ -20,14 +20,15 @@ public class RetweetFileService<E> {
                     // lets worry about getting it to print properly to the terminal and not to the file yet..
                     sb.append(retweetHashMap.get(vertex).get(i).vertex.getLabel());
                     sb.append("(").append(vertex.getWeight()).append(")").append(", ");
-                    pw.flush();
                 }
                 sb.replace(sb.length() - 2, sb.length() , "");
                 sb.append("]");
-                sb.append("\n");
+                System.out.println(sb);
+                pw.println(sb);
+                sb.setLength(0);
+                pw.flush();
+
             }
-            System.out.println(sb);
-            pw.println(sb);
 
         } catch (IOException e) {
             e.printStackTrace();
