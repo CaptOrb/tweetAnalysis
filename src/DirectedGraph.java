1984a3181c46f111b5c077019ef97a37bae96ed4
tom <tom.higgins@ucdconnect.ie>
2022-04-01 14:58:06 +0100

    Adjustment to graph interface

--- src/DirectedGraph.java
+++ src/DirectedGraph.java
@@ -1,9 +1,6 @@
 public interface DirectedGraph<T> {
     public void addArc(Vertex<T> source, Arc<T> arc);
-    //public void removeVertex(Vertex<T> vertex);
-
     void removeArc(Vertex<T> vertex, Arc<T> arc);
-
-    //public void addEdge(Vertex<T> vertex1, Vertex<T> vertex2);
-    //public void removeEdge(Vertex<T> vertex1, Vertex<T> vertex2);
+    public int getLabelBetweenVertices(Vertex<T> vertex1, Vertex<T> vertex2);
+    public boolean hasArcBetween(Vertex<T> vertex1, Vertex<T> vertex2);
 }
