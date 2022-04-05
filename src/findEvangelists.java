5a28403aadbe841833d06c88b411080adbff5046
jobrien <jane.obrien@ucdconnect.ie>
2022-04-05 13:41:31 +0100

    evangelists

--- src/findEvangelists.java
+++ src/findEvangelists.java
@@ -1,2 +1,10 @@
+import java.util.Map;
+
 public class findEvangelists {
+
+    public void top100(RetweetGraph<String> retweetedGraph){
+        //Map<String, Vertex<String>> allVerticesInGraph = retweetedGraph.getAllVerticesInGraph();
+
+
+    }
 }
