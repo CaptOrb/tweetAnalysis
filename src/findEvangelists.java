be2c421cb60f9d8440901ddc11efa422b0dfc59d
jobrien <jane.obrien@ucdconnect.ie>
2022-04-05 14:39:00 +0100

    find total retweets

--- src/findEvangelists.java
+++ src/findEvangelists.java
@@ -3,18 +3,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class findEvangelists {
+    Map<Vertex<String>, Integer > retweetsHashMap = new HashMap<>();
     Map<Vertex<String>, Integer > evangelistsHashMap = new HashMap<>();
-    public void top100(RetweetGraph<String> retweetedGraph){
+
+    public void findTotalRetweets(RetweetGraph<String> retweetedGraph){
         int totalRetweets=0;
+        int lowestInfluence=0;
         for(Vertex<String> vertex : retweetedGraph.getGraph().keySet()){
-            System.out.println(vertex);
+
             for (int i = 0; i < retweetedGraph.getGraph().get(vertex).size(); i++){
                totalRetweets+=retweetedGraph.getGraph().get(vertex).get(i).getWeight();
             }
+            System.out.println(vertex);
             System.out.println(totalRetweets);
-            evangelistsHashMap.put(vertex,totalRetweets);
+            retweetsHashMap.put(vertex, totalRetweets);
+
             totalRetweets=0; //reset
         }
-
     }
 }
