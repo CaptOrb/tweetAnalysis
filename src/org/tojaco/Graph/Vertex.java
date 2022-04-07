d42da69ab275a15283d2543b8f297afaedf5fcac
Tom Higgins <62621946+tomh00@users.noreply.github.com>
2022-04-07 22:27:41 +0100

    cleanup

--- src/org/tojaco/Graph/Vertex.java
+++ src/org/tojaco/Graph/Vertex.java
@@ -3,10 +3,14 @@ package org.tojaco.Graph;
 public class Vertex<T> {
     private final T label;
     private int stance;
+    boolean stanceSet = false;
 
     public int getStance(){ return stance; }
 
-    public void setStance(int stance){ this.stance = stance; }
+    public void setStance(int stance){
+        this.stance = stance;
+        stanceSet = true;
+    }
 
     public Vertex(T label) {
         this.label = label;
--- src/org/tojaco/Main.java
+++ src/org/tojaco/Main.java
@@ -101,7 +101,6 @@ public class Main {
                 for( int i =0; i < 20; i++) {
                     graphAnalyser.assignUserStances(rtGraph);
                 }
-                System.out.println("fkldhng");
                 break;
         }
 
