da06b0eb8c6b7db1764b32295dbc6fe1ddc31fec
jobrien <jane.obrien@ucdconnect.ie>
2022-04-08 14:40:13 +0100

    finds 100 users to check stance

--- src/org/tojaco/MainUtil.java
+++ src/org/tojaco/MainUtil.java
@@ -5,6 +5,7 @@ import org.tojaco.Graph.Arc;
 import org.tojaco.Graph.RetweetGraph;
 import org.tojaco.Graph.Vertex;
 import org.tojaco.GraphAnalysis.RetweetGraphAnalyser;
+import org.tojaco.GraphAnalysis.Users100;
 import twitter4j.TwitterFactory;
 
 import java.io.File;
@@ -95,6 +96,9 @@ public class MainUtil {
 
                 // get coverage of stances
                 graphAnalyser.calculateCoverage(rtGraph);
+
+                Users100 users100 = new Users100();
+                users100.checkStance(rtGraph);
                 break;
         }
 
