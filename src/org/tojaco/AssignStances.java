83916abbc1dcc49a09cfd312d12ccf5a37c3149d
conor.barry <conor.barry@ucdconnect.ie>
2022-04-08 13:51:44 +0100

    equal amount of pros and anti stances now

--- src/org/tojaco/AssignStances.java
+++ src/org/tojaco/AssignStances.java
@@ -2,13 +2,17 @@ package org.tojaco;
 
 import org.tojaco.Graph.Vertex;
 
-import java.io.*;
-import java.util.Map;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class AssignStances {
 
     public void determineProAntiVaxEvangelists(TwitterUsers<String> twitterUsers, File file) {
 
+        int posFound = 0;
+        int negFound = 0;
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line;
             Vertex<String> vertexError = null;
@@ -21,13 +25,15 @@ public class AssignStances {
 
                     vertexError = vertex;
 
-                    if (lineContents[1].equals("anti")) {
+                    if (lineContents[1].equals("anti") && negFound < 56) {
                         if (vertex.getLabel().equals(lineContents[0])) {
                             vertex.setStance(-1000);
+                            negFound++;
                         }
-                    } else if (lineContents[1].equals("pro")) {
+                    } else if (lineContents[1].equals("pro") && posFound < 56) {
                         if (vertex.getLabel().equals(lineContents[0])) {
                             vertex.setStance(1000);
+                            posFound++;
                         }
                     } //if it equals idk then skip
                 }
