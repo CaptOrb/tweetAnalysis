26943cf1766bf4fe7181ea49f7e76717665210bc
jobrien <jane.obrien@ucdconnect.ie>
2022-04-12 13:06:58 +0100

    silly mistake removed

--- src/org/tojaco/FileIO/ReadHashtags.java
+++ src/org/tojaco/FileIO/ReadHashtags.java
@@ -17,17 +17,16 @@ public class ReadHashtags {
                 String[] lineContents = line.split("\t");
                 try {
                     Long.parseLong(lineContents[0]);
-                    String[] findRetweet = lineContents[2].split(" "); //lineContents[2] is "RT @RetweetedUser tweetText" if it's a retweet
+                    //String[] findRetweet = lineContents[2].split(" ");
                     //if (findRetweet[2].contains("#")) { //if tweet has hashtags
-                        String[] tweetText = findRetweet[2].split(" "); //split the tweet text
+                        String[] tweetText = lineContents[2].split(" "); //split the tweet text
                         StringBuilder hashtagsInLine = new StringBuilder("");
                         for(int i = 0; i<tweetText.length; i++){
-                            if(tweetText[i].startsWith("#"));
-                            hashtagsInLine.append("\t").append(tweetText[i]); //adds username plus hashtags
+                            if(tweetText[i].startsWith("#"))
+                            hashtagsInLine.append("\t").append(tweetText[i]); //appends \t and the hashtag
                         }
-                        hashtags.add(lineContents[1] + hashtagsInLine );
-                        System.out.println(lineContents[1] + hashtagsInLine);
-                        //retweets.add(lineContents[1] + "\t" + username); //adds @User + "\t" + @RetweetedUser and whatever they tweeted
+                        hashtags.add(lineContents[1] + hashtagsInLine);
+                        System.out.println(lineContents[1] + hashtagsInLine + tweetText.length);
                     //}
                 } catch (NumberFormatException | ArrayIndexOutOfBoundsException e) {
                     j = "doNothing";
