24920f6450dafd18067eef822046e59a85be9540
jobrien <jane.obrien@ucdconnect.ie>
2022-04-22 13:52:26 +0100

    splits by indefinite article A

--- src/org/tojaco/HashtagSplitter.java
+++ src/org/tojaco/HashtagSplitter.java
@@ -22,15 +22,40 @@ public class HashtagSplitter<T, E> {
         for (Vertex<Hashtag> hashtag : hashtagToUsers.getGraph().keySet()) {
             String hashtagWord[] = hashtag.toString().split("(?<!(^|[A-Z]))(?=[A-Z])|(?<!^)(?=[A-Z][a-z])");
             for(int i=0; i<hashtagWord.length;i++){
-                if(!(hashtagWord[i].contains("…"))){
+                if(hashtagWord[i].startsWith("A") && (hashtagWord[i].length())!=1) {
+
+                    char secondLetter = hashtagWord[i].charAt(1);
+                    char thirdLetter = 'a';
+                    if(hashtagWord[i].length()>2) {
+                        thirdLetter = hashtagWord[i].charAt(2);
+                    }
+                    if(secondLetter>= 'A' && secondLetter <= 'Z' && thirdLetter >= 'a' && thirdLetter <='z' ){
+                        String IndefArticle = hashtagWord[i].substring(0,1); //start index is inclusive, end index is exclusive
+                        String restOfWord = hashtagWord[i].substring(1);
+                        hashtag.getLabel().addWord(IndefArticle.replaceAll("[#.,]","").toLowerCase());
+                        hashtag.getLabel().addWord(restOfWord.replaceAll("[#.,]","").toLowerCase());
+                        System.out.println(IndefArticle.replaceAll("[#.,]",""));
+                        System.out.println(restOfWord.replaceAll("[#.,]",""));
+                    } else{
+                        hashtag.getLabel().addWord(hashtagWord[i].replaceAll("[#.,]","").toLowerCase());
+                        System.out.println(hashtagWord[i].replaceAll("[#.,]",""));
+                    }
+                } else{
                     hashtag.getLabel().addWord(hashtagWord[i].replaceAll("[#.,]","").toLowerCase());
-                    //System.out.println(hashtagWord[i].replaceAll("[#.,]",""));
+                    System.out.println(hashtagWord[i].replaceAll("[#.,]",""));
                 }
 
-                //System.out.print(hashtagWord[i].replaceAll("#",""));
             }
-            //System.out.println();
+            System.out.println();
         }
+    }
+
+
+    //split by A for FauciIsAHero, so after an A is found and its followed by one capital letter then split it (must be followed by other letters)
+    // but PNAS shouldn't be split, regex for if A is followed by only ONE capital letter then don't split it
+
+    public void splitByA(Vertex<Hashtag> hashtag, String str){
+        //if(str.contain)
     }
 
     public boolean breakHashTagByLexicon(String hashTag, Set<String> lexiconDictionary) {
