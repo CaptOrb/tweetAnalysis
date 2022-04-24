f2717badf4f63a205bc04e3be09859403c363371
Tom Higgins <62621946+tomh00@users.noreply.github.com>
2022-04-24 09:08:39 +0100

    implementation of isFocusedOn method

--- src/org/tojaco/HashtagSummarizer.java
+++ src/org/tojaco/HashtagSummarizer.java
@@ -62,4 +62,27 @@ public class HashtagSummarizer<T,E> {
             }
         }
     }
+
+    // returns 1 if a given hashtag focuses more on property1
+    // returns 2 if a given hashtag focuses more on property2
+    // returns 0 if a given hashtag focuses more on neither properties
+    public int isFocusedOn(Hashtag hashtag, DirectedGraph<Hashtag, String> hashtagAnalyses, String property1, String property2, GraphElements graphElements){
+        Vertex<Hashtag> vertex = graphElements.getVertex(hashtag.toString(), new CreateHashtagVertex());
+        int property1Count = 0, property2Count = 0;
+        for( Arc<String> arc : hashtagAnalyses.getGraph().get(vertex)) {
+            if ( arc.getVertex().getLabel().equals(property1)){
+                property1Count = arc.getWeight();
+            }
+            else if ( arc.getVertex().getLabel().equals(property2) ){
+                property2Count = arc.getWeight();
+            }
+        }
+        if( property1Count > property2Count ){
+            return 1;
+        }
+        else if ( property2Count > property1Count ){
+            return 2;
+        }
+        return 0;
+    }
 }
