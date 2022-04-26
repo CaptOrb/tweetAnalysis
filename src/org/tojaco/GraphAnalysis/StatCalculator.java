5e38bbf527455e7377b066ca7ab5c86d709f4aac
conor.barry <conor.barry@ucdconnect.ie>
2022-04-26 15:00:40 +0100

    start Automating the calculation of many conditional probabilities

--- src/org/tojaco/GraphAnalysis/StatCalculator.java
+++ src/org/tojaco/GraphAnalysis/StatCalculator.java
@@ -7,6 +7,7 @@ import org.tojaco.GraphElements.Hashtag;
 import org.tojaco.GraphElements.TwitterUser;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
@@ -35,7 +36,7 @@ public class StatCalculator {
     }
 
 
-    public double calculateConditionalProbability(DirectedGraph<TwitterUser, String> userModel, String prop1, String prop2) {
+    public double calConditionalProbabilityWithProps(DirectedGraph<TwitterUser, String> userModel, String prop1, String prop2) {
 
         Set<Vertex<TwitterUser>> userList = userModel.getGraph().keySet();
 
@@ -48,4 +49,23 @@ public class StatCalculator {
         return probUserWithBothProps / probUserWithPropTwo;
     }
 
+    public void calConditionalProbability(DirectedGraph<TwitterUser, String> userModel) {
+
+        // TODO ignore Z-Score of < 2
+
+        Collection<ArrayList<Arc<String>>> allFeatures = userModel.getGraph().values();
+
+        for (ArrayList<Arc<String>> stringArc : allFeatures) {
+
+            for (int i = 0; i < stringArc.size(); i++) {
+                for (int j = 1; j < stringArc.size(); j++) {
+
+                    calConditionalProbabilityWithProps(userModel, stringArc.get(i).getVertex().getLabel(),
+                            stringArc.get(j).getVertex().getLabel());
+                }
+            }
+        }
+    }
 }
+
+
